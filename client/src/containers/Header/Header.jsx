364d4939f93ed5d883eb5387b8ef3aea7a56f81f
josephagbogu <73303046+josephagbogu@users.noreply.github.com>
2021-03-14 10:11:22 -0400

    - This is a temp branch (Please do not use)

--- client/src/containers/Header/Header.jsx
+++ client/src/containers/Header/Header.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import Button from '../Button';
 import { startSimulation, togglePoke, loadCircuit, saveCircuit } from '../../store/command/actions';
-import { headerBackgroundColor, headerHeight } from '../../util/style';
+import { headerBackgroundColor, headerHeight } from '../../util/style'
 
 /**
  * The header bar in the web app, load this with buttons that the user can use to
@@ -17,12 +17,22 @@ const headerStyle = {
   alignItems: 'center',
 };
 
+
+// TODO: you need to modify this and replace the const fileName
+// with the fileName specified by the user
 const Header = (props) => (
   <div style={{ ...headerStyle }}>
+    {/* // put a place for user to type filename, and when save is pressed, it takes the filename from there */}
     <Button text="TOGGLE POKE" onClick={() => props.togglePoke()} />
     <Button text="SIMULATE" onClick={() => props.startSimulation()} />
-    <Button text="LOAD CIRCUIT" onClick={() => props.loadCircuit()} />
-    <Button text="SAVE CIRCUIT" onClick={() => props.saveCircuit()} />
+    <Button
+      text="LOAD CIRCUIT" 
+      onClick={(fileName=prompt('load file')) => props.loadCircuit(fileName)}
+    />
+    <Button
+      text="SAVE CIRCUIT"
+      onClick={(fileName= prompt('save file as')) => props.saveCircuit(fileName)} 
+    />
   </div>
 );
 
