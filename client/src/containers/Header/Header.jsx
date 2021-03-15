675152954db82a39bedd4e268316eb6a73720290
stephen <fagberien@yahoo.com>
2021-03-14 20:34:14 -0400

    - Save multiple files function working   - TODO: pop up menu for user to select a file to load   - TODO: code clean up before merging with working branch

--- client/src/containers/Header/Header.jsx
+++ client/src/containers/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect} from 'react';
 import { connect } from 'react-redux';
 import Button from '../Button';
 import { startSimulation, togglePoke, loadCircuit, saveCircuit } from '../../store/command/actions';
@@ -18,23 +18,68 @@ const headerStyle = {
 };
 
 
-// TODO: you need to modify this and replace the const fileName
-// with the fileName specified by the user
-const Header = (props) => (
-  <div style={{ ...headerStyle }}>
-    {/* // put a place for user to type filename, and when save is pressed, it takes the filename from there */}
-    <Button text="TOGGLE POKE" onClick={() => props.togglePoke()} />
-    <Button text="SIMULATE" onClick={() => props.startSimulation()} />
-    <Button
-      text="LOAD CIRCUIT" 
-      onClick={(fileName=prompt('load file')) => props.loadCircuit(fileName)}
-    />
-    <Button
-      text="SAVE CIRCUIT"
-      onClick={(fileName= prompt('save file as')) => props.saveCircuit(fileName)} 
-    />
-  </div>
-);
+const Header = (props) => {
+  const localStorageKey = 'myValueInLocalStorage';
+  const circuitNamesKey = 'circuitNames';
+
+  const [fileName, setFileName] = useState(
+    localStorage.getItem(localStorageKey) || ''
+  );
+  
+  // might be better to use useRef here
+  const [isSaveClicked, setIsSaveClicked] = useState(false);
+
+  const onChange = event => {
+    isSaveClicked && setIsSaveClicked(false);
+    setFileName(event.target.value)
+  };
+  
+  const onSaveClicked = fileName => {
+    setIsSaveClicked(true);
+    return props.saveCircuit(fileName)
+  };
+
+  const onLoadClicked = fileName => {
+    // use updatedCircuitNames to get the circuits saved
+    // in here, give the user the option to pick from the list of circuits saved 
+    // using a pop up option of the list, or a dropdown menu of the list 
+    // then load the circuit which the user has selected
+    // so fileName will be equal to the selected option from the user
+    console.log(localStorage.getItem(circuitNamesKey))
+  }
+
+
+  useEffect(() => {
+    if (!fileName || !fileName.trim() || !isSaveClicked){
+      return;
+    }
+
+    const circuitNames = JSON.parse(localStorage.getItem(circuitNamesKey)) || [];
+    const updatedcircuitNames = circuitNames
+      .filter((currFileName) => currFileName !== fileName);
+    updatedcircuitNames.push(fileName);
+    
+    localStorage.setItem(circuitNamesKey, JSON.stringify(updatedcircuitNames));
+    localStorage.setItem(localStorageKey, fileName);
+  }, [isSaveClicked]);
+
+  return (
+    <div style={{ ...headerStyle }}>
+      <input value={fileName} type="text" onChange={onChange} />
+      <Button text="TOGGLE POKE" onClick={() => props.togglePoke()} />
+      <Button text="SIMULATE" onClick={() => props.startSimulation()} />
+      <Button
+        text="LOAD CIRCUIT" 
+        // onClick={onLoadClicked}
+        onClick={(fileName) => props.loadCircuit(fileName)}
+      />
+      <Button
+        text="SAVE CIRCUIT"
+        onClick={onSaveClicked} 
+      />
+    </div>
+  );
+};
 
 const mapDispatchToProps = {
   startSimulation,
